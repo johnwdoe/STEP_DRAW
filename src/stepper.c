929607cc2030b251229ce786eb975ada0363b062
johnwdoe <john_w_doe@mail.ru>
2016-10-27 15:43:42 +0300

    stepper_do

--- src/stepper.c
+++ src/stepper.c
@@ -5,4 +5,37 @@
  *      Author: ikhatckevich
  */
 
+#include "stepper.h"
+#include <avr/io.h>
+#include <avr/pgmspace.h>
 
+const PROGMEM uint8_t phaseMap[] = {
+		0b10011001,
+		0b10001000,
+		0b11001100,
+		0b01000100,
+		0b01100110,
+		0b00100010,
+		0b00110011,
+		0b00010001
+};
+
+drvCtx drvArr[] = {
+		{0, 0, 0, 1, &PORTC, 0x0F},
+		{0, 0, 0, -1, &PORTC, 0xF0}
+};
+
+uint8_t stepper_do(drvCtx* drv){
+	if (drv->curPos == drv->dstPos) return 0;
+	if (drv->dstPos > drv->curPos){
+		//move forward
+		drv->curPos++;
+		drv->phase += drv->fwdPhaseDir; //инкремент фазы с поправкой на ориентацию двигателя
+	}else{
+		//move backward
+		drv->curPos--;
+		drv->phase -= drv->fwdPhaseDir;
+	}
+	*drv->ctlPort = (*drv->ctlPort & ~drv->ctlMask) | (pgm_read_byte(&phaseMap[drv->phase & 7]) & drv->ctlMask);
+	return 1;
+}
--- src/stepper.h
+++ src/stepper.h
@@ -11,12 +11,21 @@
 #include <stdio.h>
 
 typedef struct {
-	uint8_t phase;
-	uint16_t curPos;
-	uint16_t destPos;
-	volatile uint8_t* ctlPort;
-	const uint8_t ctlMask;
-	const uint8_t ctlOffset;
+	volatile uint8_t phase; //номер текущей фазы (0-7)
+	volatile uint16_t curPos; //текущая позиция (в шагах)
+	uint16_t dstPos; //позиция назначения
+	const int8_t fwdPhaseDir; //приращение фазы, соответствующее движению вперед (+/- 1)
+	volatile uint8_t* ctlPort; //ссылка на порт (передается как &PORTx)
+	const uint8_t ctlMask; //маска части порта (0x0F, 0xF0)
+	//const uint8_t ctlOffset; //смещение... хз как объяснить. может выпилю к херам
 }drvCtx;
+
+void stepper_init (void);
+
+/*
+ * производит один шаг к позиции назначения
+ * возвращает число шагов. 0 - если уже в позиции назначения
+ * */
+uint8_t stepper_do(drvCtx* drv);
 
 #endif /* STEPPER_H_ */
